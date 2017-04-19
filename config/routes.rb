8a4e68556cb045248fbaffc5e706f8a1b77f22a1
Takashi_Furuya <takashi.furuya.0@gmail.com>
2017-04-19 08:13:04 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# Toppages
 	root to: "toppages#index"
 
+# Sessions
 	get "login", to: "sessions#new"
 	post "login",to: "sessions#create"
 	delete "logout", to: "sessions#destroy"
 
+# Users
 	get "signup", to: "users#new"
 	resources :users, only: [:index, :show, :new, :create]
+
+# Microposts
+	resources  :microposts, only: [:create, :destroy]
 end
